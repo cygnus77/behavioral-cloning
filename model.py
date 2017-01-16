85a0e3dc0fef31a4a6e2b9a4b93293ca6b7bfc08
cygnus77 <ananddinakar@gmail.com>
2017-01-16 14:31:25 -0500

    updated model to take command line args

--- model.py
+++ model.py
@@ -4,9 +4,12 @@ Model
 import os
 import csv
 import math
+import argparse
 import numpy as np
 import cv2
+import json
 import tensorflow as tf
+from keras.layers.core import Lambda
 from keras.models import Sequential, load_model
 from keras.layers import Dense, Input, Activation, Dropout, Conv2D, Flatten, MaxPooling2D, Convolution2D
 from keras.layers.advanced_activations import ELU
@@ -17,18 +20,22 @@ import matplotlib.pyplot as plt
 
 DATA_DIR = 'data/'
 LABELS_FILENAME = os.path.join(DATA_DIR,'driving_log.csv')
-MODEL_FILE = 'data/model.h5'
 
 """
 Functions for training
 """
 # generate training data in an infinite loop - passed into keras fit_generator
-def genTrainingData(data):
+def genTrainingData(data, gray):
     while True:
         data = shuffle(data)
-        src = genNormalizedData(genAugmentedViews(data))
+        src = genNormalizedData(genAugmentedViews(data), gray)
         for sample in src:
             yield sample
+
+def genValidationData(data):
+    while True:
+        for row in data:
+            yield (readImage(row['center']), float(row['steering']))
 
 # enumerate rows of csv data - returns raw row contents
 def enumDrivingLog(driving_log_fname):
@@ -44,6 +51,25 @@ def filterDrivingLog(src, threshold, keepPercentage):
         if abs(steering) > threshold or np.random.randint(0,100) <= keepPercentage:
             yield row
 
+def genShadowsOnView(src):
+    for img, steering in src:
+        # add random shadows to center, right and left cameras
+        yield (addShadow(img), steering)
+
+def genDimmerViews(src):
+    for img, steering in src:
+        # vary image brightness on each camera - to half and third of the original brightness
+        yield (adjustBrightness(img, .75), steering)
+        yield (adjustBrightness(img, 1.25), steering)
+
+def genShiftedViews(src):
+    for img, steering in src:
+        # add random shifts
+        yield addShift(img, steering, axis=0)
+        yield addShift(img, steering, axis=1)
+        # flip center camera image
+        yield (cv2.flip(img, 1), steering * -1.)
+            
 # generate images that supplement the data set
 def genAugmentedViews(src):
     for row in src:
@@ -52,53 +78,54 @@ def genAugmentedViews(src):
         left = readImage(row['left'])
         right = readImage(row['right'])
         
+        imgSrcArr = []
         # center camera image
-        yield (center, steering)
-        
+        imgSrcArr.append( (center, steering) )
+
         # adjust steering for left and right cameras
-        left_steer = np.clip(steering + 0.12, -1, 1)        
+        left_steer = np.clip(steering + 0.12, -1, 1)
         right_steer = np.clip(steering - 0.12, -1, 1)
-        yield (left, left_steer)
-        yield (right, right_steer)
+        imgSrcArr.append( (left, left_steer) )
+        imgSrcArr.append( (right, right_steer) )
 
-        # add random shadows to center, right and left cameras
-        yield (addShadow(center), steering)
-        yield (addShadow(left), left_steer)
-        yield (addShadow(right), right_steer)
+        for item in imgSrcArr:
+            yield item
+        for txItem in genShiftedViews(imgSrcArr):
+            yield txItem
+        for txItem in genDimmerViews(imgSrcArr):
+            yield txItem
+        for txItem in genShadowsOnView(imgSrcArr):
+            yield txItem
+        for txItem in genShadowsOnView(genShiftedViews(imgSrcArr)):
+            yield txItem
         
-        # add random shifts
-        yield addShift(center, steering)
-        yield addShift(left, left_steer)
-        yield addShift(right, right_steer)
-
-        # flip center camera image
-        yield (cv2.flip(center, 1), steering * -1.)
-
-        # vary image brightness on each camera - to half and third of the original brightness
-        yield (adjustBrightness(center, .75), steering)
-        yield (adjustBrightness(center, 1.25), steering)
-        yield (adjustBrightness(left, .75), left_steer)
-        yield (adjustBrightness(left, 1.25), left_steer)
-        yield (adjustBrightness(right, .75), right_steer)
-        yield (adjustBrightness(right, 1.25), right_steer)
 
 # shift image horizontally by a random number of pixels and proportionally adjust steering angle
-def addShift(img, steering):
-    axis = 1 # axis 1 for horizontal
+# axis 1 for horizontal, 0 for vertical
+def addShift(img, steering, axis = 1):
+     
     # pick random shift amount between -1/5th width to +1/5th width
-    amt = float(np.random.randint(-img.shape[axis]//5,img.shape[axis]//5+1))
+    amt = int(np.random.randint(-img.shape[axis]//5,img.shape[axis]//5+1))
     # destination image that will contain shifted image
     sImg = np.zeros(img.shape,dtype=np.uint8)
-    # compute change in steering
-    steer_inc = -amt / img.shape[axis]
-    #print("Steering: %.3f, Inc: %.3f" %(steering, steer_inc))
-    steering += steer_inc
-    # shift image by copying part of image into destination
-    if amt < 0:
-        amt = abs(amt)
-        sImg[:,amt:img.shape[axis]] = img[:,0:img.shape[axis]-amt]
+    if axis == 1:
+        # compute change in steering
+        steer_inc = -amt / img.shape[axis]
+        #print("Steering: %.3f, Inc: %.3f" %(steering, steer_inc))
+        steering += steer_inc
+        # shift image by copying part of image into destination
+        if amt < 0:
+            amt = abs(amt)
+            sImg[:,amt:img.shape[axis]] = img[:,0:img.shape[axis]-amt]
+        else:
+            sImg[:,0:img.shape[axis]-amt] = img[:,amt:img.shape[axis]]
     else:
-        sImg[:,0:img.shape[axis]-amt] = img[:,amt:img.shape[axis]]
+        # shift image by copying part of image into destination
+        if amt < 0:
+            amt = abs(amt)
+            sImg[amt:img.shape[axis],:] = img[0:img.shape[axis]-amt,:]
+        else:
+            sImg[0:img.shape[axis]-amt,:] = img[amt:img.shape[axis],:]
     # return new image and steering pair
     return sImg,steering
 
@@ -119,7 +146,7 @@ def addShadow(img):
     for j in range(mask.shape[0]):
         for i in range(mask.shape[1]):
             if j > (i*slope)+intercept:
-                mask[j,i] -= 80
+                mask[j,i] -= np.random.randint(30,180)
     # apply mask
     y += mask
     # ensure values are within uint8 range to avoid artifacts
@@ -136,15 +163,14 @@ def adjustBrightness(img, m):
 # normalize image and steering data
 # round out steering values to fall in 0.01 buckets (so problem is simplified, could even become classification problem)
 # keras requires np.array for y values: convert steering to 1D np.array
-def genNormalizedData(src):
+def genNormalizedData(src, gray):
     for img,steering in src:
-        # round and convert from -1 : +1 to -0.5 : + 0.5
-        #normSteering = round(steering,2) / 2.
+        # round steering values
         normSteering = round(steering,2)
-        x = normalizeImage(img)
+        x = normalizeImage(img, gray)
         y = np.array(normSteering, ndmin=1, dtype=np.float)
         yield x,y
-        
+
 """
 Operations done for every image - both training and predictions
 """
@@ -172,21 +198,22 @@ def cropImage(img):
     return img[cropHt:ht-25, :]
 
 # normalize image data
-# convert to y,u,v
+# convert to y,u,v or gray (keep only y)
 # crop to remove top 5th and bottom 5th of image
-# adjust pixel values to fall within -1 to +1
 # keras requires 4d-tensor input: add dimension 1, h,w,d for image data
-def normalizeImage(img):
+def normalizeImage(img, gray=False):
     croppedImg = cropImage(img)
     resizedImg = cv2.resize(croppedImg, (200,66), interpolation = cv2.INTER_CUBIC)
-    yuvImg = convColorSpace2YUV(resizedImg)
-    nImg = ((yuvImg / 127.5) - 1.).astype(np.float32)
-    x = nImg.reshape(1,*nImg.shape)
+    if gray:
+        yuvImg = convColorSpace2Gray(resizedImg)
+    else:
+        yuvImg = convColorSpace2YUV(resizedImg)
+    x = yuvImg.reshape(1,*yuvImg.shape).astype(np.float32)
     return x
 
 # de-normalize image data to RGB format for display
 def imgForDisplay(img):
-    img = ((img + 1.) * 127.5).astype(np.uint8)
+    img = img.astype(np.uint8)
     img = img.reshape(img.shape[1:4])
     if img.shape[2] == 3: #yuv
         img = cv2.cvtColor(img, cv2.COLOR_YCrCb2RGB)
@@ -194,69 +221,48 @@ def imgForDisplay(img):
         img = img.reshape(img.shape[0:2])
     return img
 
-# Parse csv file, select rows for training
-data = list(filterDrivingLog(enumDrivingLog(LABELS_FILENAME), 0.01, 25))
-
-# split into train and validation
-#train, test = train_test_split(data, test_size=0.25)
-train = shuffle(data)
-
-# select a row that has non-zero steering angle to be used for visualization and computing
-# image dimensions and samples per image created by augmentation
-sampleIdx = 0
-while float(train[sampleIdx]['steering']) == 0.:
-    sampleIdx += 1
-
-# wrap selected row with data augmentation generators
-imageGenerator = genNormalizedData(genAugmentedViews([train[sampleIdx]]))
-imgListExample = list(imageGenerator)
-numImagesPerSample = len(imgListExample)
-numRows = len(train) * numImagesPerSample
-exampleImg = imgListExample[0][0]
-
-# get image dimensions
-image_shape = exampleImg.shape
-print("NumSamples: {0}, Shape:{1}".format(numRows, image_shape))
-
-# Visualization of selected - to ensure augmented images are correct
-#for normImg,normSteering in imgListExample:
-#    plt.figure()
-#    plt.axis('off')
-#    img = imgForDisplay(normImg)
-#    ht = img.shape[0]+10
-#    steering = normSteering # * 2.
-#    if len(img.shape) == 3: # rgb
-#        plt.imshow(img)
-#    else: # grayscale
-#        plt.imshow(img, cmap='gray')
-#    plt.text(0,ht,'Steering: %.3f' % steering)
-
 # Model based on NVIDIA model
 # Dropouts are not specified in the paper
-def nvidia_model():
+def nvidia_model(input_shape, use_dropout = True):
     model = Sequential()
-    model.add(Convolution2D(24, 5, 5, subsample=(2,2), border_mode='same', init='he_normal', input_shape=image_shape[1:4]))
+    model.add(Lambda(lambda x: (x / 127.5) - 1.,input_shape=input_shape))
+    
+    model.add(Convolution2D(24, 5, 5, subsample=(2,2), border_mode='same', init='he_normal'))
     model.add(ELU())
+    if use_dropout:
+        model.add(Dropout(0.1))
     
     model.add(Convolution2D(36, 5, 5, subsample=(2,2), border_mode='same', init='he_normal'))
     model.add(ELU())
+    if use_dropout:
+        model.add(Dropout(0.25))
     
-    model.add(Convolution2D(48, 5, 5, subsample=(2,2), border_mode='valid', init='he_normal'))
+    model.add(Convolution2D(48, 5, 5, subsample=(2,2), border_mode='same', init='he_normal'))
     model.add(ELU())
+    if use_dropout:
+        model.add(Dropout(0.25))
     
-    model.add(Convolution2D(64, 3, 3, subsample=(1,1), border_mode='valid', init='he_normal'))
+    model.add(Convolution2D(64, 3, 3, subsample=(2,2), border_mode='same', init='he_normal'))
     model.add(ELU())
+    if use_dropout:
+        model.add(Dropout(0.5))
     
-    model.add(Convolution2D(64, 3, 3, subsample=(1,1), border_mode='valid', init='he_normal'))
+    model.add(Convolution2D(64, 3, 3, subsample=(2,2), border_mode='same', init='he_normal'))
     model.add(ELU())
+    if use_dropout:
+        model.add(Dropout(0.5))
     
     model.add(Flatten())
     
     model.add(Dense(100, init='he_normal'))
     model.add(ELU())
+    if use_dropout:
+        model.add(Dropout(0.5))
     
     model.add(Dense(50, init='he_normal'))
     model.add(ELU())
+    if use_dropout:
+        model.add(Dropout(0.25))
     
     model.add(Dense(10, init='he_normal'))
     model.add(ELU())
@@ -265,25 +271,94 @@ def nvidia_model():
     
     return model
 
-# Load existing model & weights from disk or create a new model
-if os.path.exists(MODEL_FILE):
-    print("Loading from file")
-    model = load_model(MODEL_FILE)
-else:
-    print("Creating model")
-    model = nvidia_model()
+if __name__ == '__main__':
 
-# print model summary
-model.summary()
+    parser = argparse.ArgumentParser(description='Train NVIDIA Model on Udacity data')
+    parser.add_argument('model', help='file name where model is stored. Extension must be .h5, will be added if not provided')
+    parser.add_argument('-g', dest='gray', help='Convert images to grayscale', action='store_true')
+    parser.add_argument('-v', dest='validation', help='Separate data into train and validation', action='store_true')
+    parser.add_argument('-d', dest='dropout', help='Use dropouts', action='store_true')
+    parser.add_argument('-p', dest='print', help='Display augmented samples', action='store_true')
+    args = parser.parse_args()
+    
+    # Get file path to model
+    model_file = os.join(DATA_DIR, args.model)
+    if not model_file.endswith('.h5'):
+        model_file += '.h5'
+    
+    # Parse csv file, select rows for training
+    data = list(filterDrivingLog(enumDrivingLog(LABELS_FILENAME), 0.01, 25))
 
-# Train
-optimizer = Adam(lr=0.0001)
-model.compile(loss='mean_squared_error', optimizer=optimizer, metrics=['accuracy'])
-history = model.fit_generator(genTrainingData(train), samples_per_epoch=numRows, nb_epoch=10, verbose=1, max_q_size=100)
+    if args.validation:
+        # split into train and validation
+        train, val = train_test_split(shuffle(data), test_size=0.20)
+    else:
+        # use all the data for final training
+        train = shuffle(data)
+        val = None
 
-# Save weights and model json file
-model.save(MODEL_FILE)
-jsonStr = model.to_json()
-with open(MODEL_FILE.replace('h5','json'), 'w') as jf:
-    jf.write(jsonStr)
+    # select a row that has non-zero steering angle to be used for visualization and computing
+    # image dimensions and samples per image created by augmentation
+    sampleIdx = 0
+    while float(train[sampleIdx]['steering']) == 0.:
+        sampleIdx += 1
 
+    # wrap selected row with data augmentation generators
+    imageGenerator = genNormalizedData(genAugmentedViews([train[sampleIdx]]))
+    imgListExample = list(imageGenerator)
+    numImagesPerSample = len(imgListExample)
+    numRows = len(train) * numImagesPerSample
+    exampleImg = imgListExample[0][0]
+
+    # get image dimensions
+    image_shape = exampleImg.shape
+    print("NumSamples: {0}, Shape:{1}".format(numRows, image_shape))
+
+    if args.display:
+        # Visualization of selected - to ensure augmented images are correct
+        for normImg,normSteering in imgListExample:
+            plt.figure()
+            plt.axis('off')
+            img = imgForDisplay(normImg)
+            ht = img.shape[0]+10
+            steering = normSteering # * 2.
+            if len(img.shape) == 3: # rgb
+                plt.imshow(img)
+            else: # grayscale
+                plt.imshow(img, cmap='gray')
+            plt.text(0,ht,'Steering: %.3f' % steering)
+
+    # Load existing model & weights from disk or create a new model
+    if os.path.exists(model_file):
+        print("Loading from file")
+        model = load_model(model_file)
+    else:
+        print("Creating model")
+        model = nvidia_model(image_shape[1:4], args.dropout)
+
+    # print model summary
+    model.summary()
+
+    # Train
+    optimizer = Adam(lr=0.0001)
+    model.compile(loss='mean_squared_error', optimizer=optimizer)
+    if val == None:
+        history = model.fit_generator(genTrainingData(train, args.gray), samples_per_epoch=numRows, 
+                      nb_epoch=25, verbose=1, max_q_size=256, pickle_safe=True, nb_worker=32)
+    else:
+        history = model.fit_generator(genTrainingData(train, args.gray), samples_per_epoch=numRows, 
+                      nb_epoch=25, verbose=1, max_q_size=256, pickle_safe=True, nb_worker=32,
+                      validation_data=genNormalizedData(genValidationData(val)), nb_val_samples=len(val))
+
+    # Save weights and model json file
+    model.save(model_file)
+    jsonStr = model.to_json()
+    with open(model_file.replace('h5','json'), 'w') as jf:
+        jf.write(jsonStr)
+
+    # Save learning rate history to file
+    with open(model_file.replace('.h5','.history.csv'), 'w') as hist_csv:
+        hist_wr = csv.writer(hist_csv)
+        hist_wr.writerow(history.history['loss'])
+        if args.validation:
+            hist_wr.writerow(history.history['val_loss'])
